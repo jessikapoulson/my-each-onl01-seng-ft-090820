d584517e54fd3b27a80447126c625f8f6f7f5bd2
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-18 16:16:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  arr_member = 0
+  while arr_member < array.length
+    yield(array[arr_member])
+    arr_member = arr_member + 1
+  end
+  collection
 end
